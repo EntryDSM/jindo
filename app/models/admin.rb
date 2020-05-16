ac3e2cf6c95442afb28fd47b742fa129f62cd470
JeongWooYeong <wjd030811@gmail.com>
2020-05-17 02:14:52 +0900

    [ADD] password encryption strategy

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,21 @@
 class Admin < ApplicationRecord
+  include BCrypt
+
   self.primary_key = :email
 
-  alias_attribute :password_digest, :password
+  def password_decryption
+    Password.new(password)
+  end
+
+  def password_encryption=(new_password)
+    self.password = Password.create(new_password)
+  end
+
+  def self.create!(**kwargs)
+    instance = super kwargs
+    instance.password = Password.create(instance.password)
+    instance.save
+
+    instance
+  end
 end
