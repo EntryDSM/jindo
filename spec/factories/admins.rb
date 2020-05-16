537407f690b131552fbd4d91a321da4bdae475eb
JeongWooYeong <wjd030811@gmail.com>
2020-05-17 02:17:13 +0900

    [ADD] configure when create factory object

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :admin do
-    
+    email { FFaker::Internet.free_email }
+    password { BCrypt::Password.create FFaker::Internet.password }
+    name { FFaker::NameKR.name }
   end
 end
