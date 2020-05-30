1191039db526e02de5a90c28b24b25f6428a5578
JeongWooYeong <wjd030811@gmail.com>
2020-05-31 01:25:53 +0900

    [ADD] configure when create factory object "ged"

--- spec/factories/ged_applications.rb
+++ spec/factories/ged_applications.rb
@@ -1,5 +1,9 @@
+require_relative 'factory_bot_helpers'
+
 FactoryBot.define do
   factory :ged_application do
-    
+    ged_average_score { 600 }
+    created_at { Time.zone.now }
+    modified_at { Time.zone.now }
   end
 end
