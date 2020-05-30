2c899def96b79a59f1e62cf05cad925435920b57
JeongWooYeong <wjd030811@gmail.com>
2020-05-30 22:06:02 +0900

    [ADD] configure foreign key about "has" relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   self.primary_key = :email
 
-  enum apply_type: %w[MEISTER SOCIAL_ONE_PARENT SOCIAL_FROM_NORTH
+  enum apply_type: %w[COMMON MEISTER SOCIAL_ONE_PARENT SOCIAL_FROM_NORTH
                       SOCIAL_MULTICULTURAL SOCIAL_BASIC_LIVING
                       SOCIAL_LOWEST_INCOME SOCIAL_TEEN_HOUSEHOLDER]
   enum additional_type: %w[NATIONAL_MERIT PRIVILEGED_ADMISSION NOT_APPLICABLE]
   enum grade_type: %w[GED UNGRADUATED GRADUATED]
   enum sex: %w[MALE FEMALE]
 
-  has_one :ged_application
-  has_one :graduated_application
-  has_one :ungraduated_application
-  has_one :status
-  has_one :calculated_score
+  has_one :ged_application, foreign_key: :user_email
+  has_one :graduated_application, foreign_key: :user_email
+  has_one :ungraduated_application, foreign_key: :user_email
+  has_one :status, foreign_key: :user_email
+  has_one :calculated_score, foreign_key: :user_email
 end
