ff154a0fccbb0e834d60d9a53d21bb33824529e6
JeongWooYeong <wjd030811@gmail.com>
2020-05-31 01:24:28 +0900

    [ADD] configure when create factory object "calculated score"

--- spec/factories/calculated_scores.rb
+++ spec/factories/calculated_scores.rb
@@ -1,5 +1,12 @@
+require_relative 'factory_bot_helpers'
+
 FactoryBot.define do
   factory :calculated_score do
-    
+    volunteer_score { 15 }
+    attendance_score { 15 }
+    conversion_score { 150 }
+    final_score { 180 }
+    created_at { Time.zone.now }
+    modified_at { Time.zone.now }
   end
 end
