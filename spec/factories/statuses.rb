8736999325ece4ccfb3f3ed271bf8a76fbff70e6
JeongWooYeong <wjd030811@gmail.com>
2020-05-31 01:24:45 +0900

    [ADD] configure when create factory object "status"

--- spec/factories/statuses.rb
+++ spec/factories/statuses.rb
@@ -1,5 +1,13 @@
+require_relative 'factory_bot_helpers'
+
 FactoryBot.define do
   factory :status do
-    
+    is_paid { rand_boolean }
+    is_printed_application_arrived { rand_boolean }
+    is_passed_first_apply { rand_boolean }
+    is_passed_interview { rand_boolean }
+    is_final_submit { rand_boolean }
+    submitted_at { Time.zone.now }
+    exam_code { '123456' }
   end
 end
