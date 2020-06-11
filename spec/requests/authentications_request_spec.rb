b387eb9b5517ff8d49c6679e241783c7ebcfb64b
JeongWooYeong <wjd030811@gmail.com>
2020-06-11 17:44:33 +0900

    [ADD] test codes for authentication of request

--- spec/requests/authentications_request_spec.rb
+++ spec/requests/authentications_request_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe "Authentications", type: :request do
+RSpec.describe 'Authentications', type: :request do
+  describe 'POST#login' do
+    before(:all) do
+      set_database
+    end
 
+    it '> return access, refresh tokens' do
+      request('post', '/auth', { email: @admin.email,
+                                 password: @admin.password }, false)
+
+      resp = JSON.parse(response.body)
+
+      @jwt_base.refresh_token_required(resp['refresh_token'])
+      payload = @jwt_base.jwt_required(resp['access_token'])
+
+      expect(payload).to equal('email': @admin.email)
+    end
+
+    it '> invalid params' do
+      request('post', '/auth', false, false)
+
+      expect(response.status).to equal(400)
+    end
+
+    it '> invalid email/password' do
+      request('post', '/auth', { email: 'invalid@email.com',
+                                 password: 'invalid' }, false)
+
+      expect(response.status).to equal(401)
+    end
+  end
+
+  describe 'PUT#refresh' do
+    it '> return access token' do
+      request('put', '/auth', false,
+              @jwt_base.create_refresh_token(email: @admin.email))
+
+      resp = JSON.parse(response.body)
+      payload = @jwt_base.jwt_required(resp['access_token'])
+
+      expect(payload).to equal('email': @admin.email)
+    end
+
+    it '> unauthorized token' do
+      request('post', '/auth', false, false)
+
+      expect(response.status).to equal(401)
+    end
+
+    it '> invalid type of token' do
+      request('post', '/auth', false, true)
+
+      expect(response.status).to equal(403)
+    end
+  end
 end
