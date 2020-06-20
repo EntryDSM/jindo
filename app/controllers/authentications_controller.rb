f057a2cd2b1bdbf39fc78cdf633731b97c6c895f
JeongWooYeong <wjd030811@gmail.com>
2020-06-20 21:53:03 +0900

    [ADD] authentication controller codes

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,12 +1,21 @@
 class AuthenticationsController < ApplicationController
-  before_action :jwt_required, only: :login
   before_action :refresh_token_required, only: :refresh
 
   def login
+    params.require(%i[email password])
 
+    admin = Admin.find_by_email(params[:email])
+
+    return render status: :unauthorized unless admin
+    return render status: :unauthorized unless admin.password_decryption == params[:password]
+
+    render json: { access_token: @@jwt_base.create_access_token(email: admin.email),
+                   refresh_token: @@jwt_base.create_refresh_token(email: admin.email) },
+           status: :ok
   end
 
   def refresh
-
+    render json: { access_token: @@jwt_base.create_access_token(email: @payload['email']) },
+           status: :ok
   end
 end
