ffe1f38d49f461fb7a7c97c124a86c4290b40872
JeongWooYeong <wjd030811@gmail.com>
2020-06-20 20:27:41 +0900

    [ADD] statistics controller codes

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,6 +2,23 @@ class StatisticsController < ApplicationController
   before_action :jwt_required
 
   def index
+    params.require(:area)
 
+    total_applicant_count = User.count
+
+    if params[:area] == 'daejeon'
+      return render json: User.statistics(true), status: :ok
+    elsif params[:area] == 'nationwide'
+      return render json: User.statistics(false), status: :ok
+    elsif params[:area] == 'all'
+      return render json: {
+        nationwide: User.statistics(false),
+        daejeon: User.statistics(true),
+        total_applicant_count: total_applicant_count,
+        total_competition_rate: (total_applicant_count.to_r / 80).round(2).to_f
+      }, status: :ok
+    end
+
+    render status: :bad_request
   end
 end
