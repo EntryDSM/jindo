828732d83cfa0a2dbd581340ccb9e1f5d4bd528c
JeongWooYeong <wjd030811@gmail.com>
2020-07-07 11:20:59 +0900

    [FIX] function placement

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,35 +22,6 @@ class User < ApplicationRecord
   COMMON_PASSER_COUNT = 20
   TOTAL_PASSER_COUNT = 40
 
-
-  def self.score_distribution(key, value, is_daejeon)
-    (70..150).step(10).each do |i|
-      conversion_score_query = if i > 70
-                                 "conversion_score BETWEEN #{i - 9} AND #{i}"
-                               else
-                                 'conversion_score <= 70'
-                               end
-
-      applicant_count = if key != :social_applicant
-                          User.joins(:calculated_score)
-                              .where(conversion_score_query)
-                              .where(apply_type: key[0..-11].upcase,
-                                     is_daejeon: is_daejeon).count
-                        else
-                          User.joins(:calculated_score)
-                              .where.not(apply_type: %w[COMMON MEISTER])
-                              .where(conversion_score_query)
-                              .where(is_daejeon: is_daejeon).count
-                        end
-
-      if i > 70
-        value[:"#{i - 9}-#{i}"] = applicant_count
-      else
-        value[:'-70'] = applicant_count
-      end
-    end
-  end
-
   def self.statistics(is_daejeon)
     meister_applicant_count = User.where(apply_type: 'MEISTER', is_daejeon: is_daejeon).count
     common_applicant_count = User.where(apply_type: 'COMMON', is_daejeon: is_daejeon).count
@@ -92,67 +63,32 @@ class User < ApplicationRecord
     response
   end
 
-  def applicant_information
-    response = { applicant_information: {
-      status: {
-        is_paid: status.is_paid,
-        is_arrived: status.is_printed_application_arrived,
-        is_final_submit: status.is_final_submit
-      },
+  def self.score_distribution(key, value, is_daejeon)
+    (70..150).step(10).each do |i|
+      conversion_score_query = if i > 70
+                                 "conversion_score BETWEEN #{i - 9} AND #{i}"
+                               else
+                                 'conversion_score <= 70'
+                               end
 
-      privacy: {
-        user_photo: user_photo,
-        name: name,
-        birth_date: birth_date.strftime('%Y-%m-%d'),
-        grade_type: grade_type,
-        apply_type: apply_type,
-        address: address,
-        detail_address: detail_address,
-        applicant_tel: applicant_tel,
-        parent_tel: parent_tel,
-        home_tel: home_tel,
-        email: email
-      },
+      applicant_count = if key != :social_applicant
+                          User.joins(:calculated_score)
+                              .where(conversion_score_query)
+                              .where(apply_type: key[0..-11].upcase,
+                                     is_daejeon: is_daejeon).count
+                        else
+                          User.joins(:calculated_score)
+                              .where.not(apply_type: %w[COMMON MEISTER])
+                              .where(conversion_score_query)
+                              .where(is_daejeon: is_daejeon).count
+                        end
 
-      evaluation: {
-        conversion_score: calculated_score.conversion_score,
-        self_introduction: self_introduction,
-        study_plan: study_plan
-      }
-    } }
-
-    return response if grade_type == 'GED'
-
-    applicant_type = send(grade_type.downcase + '_application')
-    privacy = response[:applicant_information][:privacy]
-    evaluation = response[:applicant_information][:evaluation]
-
-    privacy[:school_name] = applicant_type.school.school_full_name
-    privacy[:school_tel] = applicant_type.school_tel
-    evaluation[:volunteer_time] = applicant_type.volunteer_time
-    evaluation[:full_absent_count] = applicant_type.full_cut_count
-    evaluation[:early_leave_count] = applicant_type.early_leave_count
-    evaluation[:late_count] = applicant_type.late_count
-    evaluation[:period_absent_count] = applicant_type.period_cut_count
-
-    response[:applicant_information][:privacy] = privacy
-    response[:applicant_information][:evaluation] = evaluation
-
-    response
-  end
-
-  def applicants_information
-    return nil if nil?
-
-    {
-      examination_number: status.exam_code,
-      name: name,
-      is_daejeon: is_daejeon,
-      apply_type: apply_type,
-      is_arrived: status.is_printed_application_arrived,
-      is_paid: status.is_paid,
-      is_final_submit: status.is_final_submit
-    }
+      if i > 70
+        value[:"#{i - 9}-#{i}"] = applicant_count
+      else
+        value[:'-70'] = applicant_count
+      end
+    end
   end
 
   def self.applicants_information(index, presence_filter = nil, filter_value = nil)
@@ -203,5 +139,68 @@ class User < ApplicationRecord
           .map(&:applicants_information)
           .compact
     end
+  end
+
+  def applicants_information
+    return nil if nil?
+
+    {
+      examination_number: status.exam_code,
+      name: name,
+      is_daejeon: is_daejeon,
+      apply_type: apply_type,
+      is_arrived: status.is_printed_application_arrived,
+      is_paid: status.is_paid,
+      is_final_submit: status.is_final_submit
+    }
+  end
+
+  def applicant_information
+    response = { applicant_information: {
+      status: {
+        is_paid: status.is_paid,
+        is_arrived: status.is_printed_application_arrived,
+        is_final_submit: status.is_final_submit
+      },
+
+      privacy: {
+        user_photo: user_photo,
+        name: name,
+        birth_date: birth_date.strftime('%Y-%m-%d'),
+        grade_type: grade_type,
+        apply_type: apply_type,
+        address: address,
+        detail_address: detail_address,
+        applicant_tel: applicant_tel,
+        parent_tel: parent_tel,
+        home_tel: home_tel,
+        email: email
+      },
+
+      evaluation: {
+        conversion_score: calculated_score.conversion_score,
+        self_introduction: self_introduction,
+        study_plan: study_plan
+      }
+    } }
+
+    return response if grade_type == 'GED'
+
+    applicant_type = send(grade_type.downcase + '_application')
+    privacy = response[:applicant_information][:privacy]
+    evaluation = response[:applicant_information][:evaluation]
+
+    privacy[:school_name] = applicant_type.school.school_full_name
+    privacy[:school_tel] = applicant_type.school_tel
+    evaluation[:volunteer_time] = applicant_type.volunteer_time
+    evaluation[:full_absent_count] = applicant_type.full_cut_count
+    evaluation[:early_leave_count] = applicant_type.early_leave_count
+    evaluation[:late_count] = applicant_type.late_count
+    evaluation[:period_absent_count] = applicant_type.period_cut_count
+
+    response[:applicant_information][:privacy] = privacy
+    response[:applicant_information][:evaluation] = evaluation
+
+    response
   end
 end
