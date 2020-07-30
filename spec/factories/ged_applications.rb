c5026a8e78b720193276ad591c4667615fba78ff
JeongWooYeong <wjd030811@gmail.com>
2020-07-30 22:30:59 +0900

    [ADD] "ged_pass_date" to ged_application table

--- spec/factories/ged_applications.rb
+++ spec/factories/ged_applications.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     ged_average_score { 600 }
     created_at { DateTime.new(2020) }
     modified_at { DateTime.new(2020) }
+    ged_pass_date { DateTime.new(2020) }
   end
 end
